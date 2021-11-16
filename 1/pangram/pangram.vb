5806242c9dec2961f5572cb6e75c55240fca0bb7
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:23:18 -0800

    Add files via upload

---
+++ 1/pangram/pangram.vb
@@ -0,0 +1,37 @@
+﻿Module pangram
+
+    Sub Main()
+        Dim str, Astr As String
+        Dim nxt As Char
+        Dim ispangram As Boolean
+
+        str = ""
+        Astr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        nxt = ""
+        ispangram = True
+
+        Console.Write("Enter string :")
+        str = Console.ReadLine
+        str = UCase(str)
+
+        For count = 1 To Len(Astr)
+            nxt = Mid(Astr, count, 1)
+            If InStr(str, nxt) = 0 Then
+                ispangram = False
+                Exit For
+
+            End If
+
+        Next
+
+        If ispangram = True Then
+            Console.WriteLine("string has all the alphabets")
+        Else
+            Console.WriteLine("string does not have all the alphabets")
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
+
