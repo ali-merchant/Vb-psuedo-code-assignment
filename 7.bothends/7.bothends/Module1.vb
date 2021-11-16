270119e5cfde83649f053c154d541a7183a223ef
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:36:41 -0800

    Add files via upload

---
+++ 7.bothends/7.bothends/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s, first, second As String
+        s = ""
+        first = ""
+        second = ""
+
+        Console.Write("input string :")
+        s = Console.ReadLine
+
+
+        If Len(s) > 4 Then
+            first = (Left(s, 2))
+
+            second = (Right(s, 2))
+
+            Console.WriteLine(first & second)
+            Console.ReadKey()
+
+        Else
+            s = "s"
+            Console.WriteLine(s)
+            Console.ReadKey()
+
+        End If
+
+
+    End Sub
+End Module
