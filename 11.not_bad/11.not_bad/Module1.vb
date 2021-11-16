29e585a609cdc9374ce8251828f651b0d6fbb9d4
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:41:00 -0800

    Add files via upload

---
+++ 11.not_bad/11.not_bad/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    Sub Main()
+
+        Dim s, newtxt, discard As String
+        Dim no, bad, in1, in2 As Integer
+        s = ""
+        newtxt = ""
+        no = 0
+        bad = 0
+
+        Console.WriteLine("input string")
+        s = LCase(Console.ReadLine())
+
+        no = InStr(1, s, "not")
+        Console.WriteLine(no)
+
+        bad = InStr(1, s, "bad")
+        bad = bad + 1
+        Console.WriteLine(bad)
+
+        in1 = no
+        in2 = bad
+
+
+        newtxt = s.Substring(in2 - in1)
+
+        discard = Replace(s, newtxt, "good")
+        Console.WriteLine(discard)
+
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
