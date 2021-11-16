79b653f48ec3378acd36f2b303a282aedc93505e
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:26:19 -0800

    Add files via upload

---
+++ 2/replace char/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str, str2 As String
+        Dim del, rep, nxt As Char
+        Str = ""
+        str2 = ""
+        del = ""
+        rep = ""
+        nxt = ""
+
+
+
+
+
+
+        Console.Write("Enter string :")
+        Str = Console.ReadLine
+
+        Console.Write("enter char to remove:")
+        del = Console.ReadLine
+
+        Console.Write("enter char to replace:")
+        rep = Console.ReadLine
+
+        For count = 1 To Len(Str)
+            nxt = Mid(Str, count, 1)
+            If nxt = del Then
+                nxt = rep
+
+            End If
+            Str2 = str2 & nxt
+        Next
+
+
+        Console.WriteLine("new string is :" & str2)
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
