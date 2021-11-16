bf16fe2d628ced71283baa2290e10a65800547c2
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:34:14 -0800

    Add files via upload

---
+++ 6 donuts/6 donuts/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim donuts As Integer = 0
+
+
+        Console.WriteLine("input number of donuts")
+
+        donuts = Console.ReadLine
+
+        If donuts < 10 Then
+            Console.WriteLine("Number of donuts :" & donuts)
+
+        Else
+
+            Console.WriteLine("Number of donuts : many")
+
+        End If
+
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+
+End Module
+
