dd92bc216a108eb89ef763fc2cd8277fc194234b
ali-merchant <93522005+ali-merchant@users.noreply.github.com>
2021-11-16 08:39:36 -0800

    Add files via upload

---
+++ 10/10/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s, r As String
+
+
+        r = ""
+        s = ""
+
+        Console.WriteLine("input string:")
+        s = Console.ReadLine()
+
+        r = Right(s, 3)
+
+        If r = "ing" Then
+            s = s & "ly"
+            Console.WriteLine(s)
+
+        elseIf Len(s) >= 3 Then
+            s = s & "ing"
+            Console.WriteLine(s)
+
+        ElseIf Len(s) < 3 Then
+
+            Console.WriteLine(s)
+
+        End If
+
+
+
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module
